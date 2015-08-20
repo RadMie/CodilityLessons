76753e3de11723dfdd5428e5f1fa968826304726
Radoslaw Mietkiewicz <radoslaw.mietkiewicz@gmail.com>
2015-08-20 16:18:25 +0200

    Update

---
+++ CodilityLessons_2_FrogRiverOne.php
@@ -0,0 +1,22 @@
+function solution($X, $A) {
+    
+    $count = 0;
+    $x = 0;
+    $array = array_fill(0, $X+1, 0); 
+     
+    foreach($A as $a) {
+    
+        if($array[$a] == 0) {
+            $array[$a] = $a;
+            
+            $x++;
+            
+            if($x == $X) {
+            
+                return $count;
+            }
+        }
+        $count++;
+    }
+    return -1;
+}
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Codility Lessons
----
+
 ### Lessons 1
 
 - FrogJmp (100/100)
 - PermMissingElem (100/100)
+
+### Lessons 2
+
+- FrogRiverOne (100/100)
